210dbecd17e95cd73042039503b21e5806357c8d
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2021-08-18 11:41:02 +0300

    Added a proper server

--- app_python/app.py
+++ app_python/app.py
@@ -1,10 +1,12 @@
+from gevent.pywsgi import WSGIServer
 from app_python import create_app
 import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    PORT = os.environ.get("PORT", "5000")
+    PORT = int(os.environ.get("PORT", "5000"))
     HOST_IP = os.environ.get("HOST_IP", "0.0.0.0")
 
-    app.run(host=HOST_IP, port=PORT)
+    http_server = WSGIServer((HOST_IP, PORT), app)
+    http_server.serve_forever()
