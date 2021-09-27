dfb176738ce30896edd5a0a67c09c03262705300
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2021-09-27 03:27:38 +0300

    Completed Lab 12

--- app_python/src/__init__.py
+++ app_python/src/__init__.py
@@ -1,7 +1,7 @@
 import datetime
 
 import pytz
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, request
 
 
 def create_app():
@@ -16,8 +16,17 @@ def create_app():
     def get_time():
         return Response(get_moscow_time(), mimetype="text")
 
+    @app.route("/visits")
+    def visits():
+        with open("/visits.txt", "r") as f:
+            c = f.readlines()
+            tag1 = '<span style="white-space: pre-line">'
+            return tag1 + "\n".join(c) + "</span>"
+
     @app.route("/")
     def main_page():
+        with open("/visits.txt", "a") as f:
+            f.write(f"[{datetime.datetime.now()}] {request.remote_addr} \n")
         return render_template("index.html", current_time=get_moscow_time())
 
     return app
